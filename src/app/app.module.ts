d61d063f2c70a709de3ac2ab435adee793bacd0f
Benjamin Bravo <benjamin.bravo@moofwd.com>
2022-06-01 14:18:23 -0400

    Beta 0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { CountdownModule } from 'ngx-countdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    CountdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CountdownComponent, CountdownConfig } from 'ngx-countdown';
+import {interval} from 'rxjs';
+
+const CountdownTimeUnits: Array<[string, number]> = [
+  ['Y', 1000 * 60 * 60 * 24 * 365], // years
+  ['M', 1000 * 60 * 60 * 24 * 30], // months
+  ['D', 1000 * 60 * 60 * 24], // days
+  ['H', 1000 * 60 * 60], // hours
+  ['m', 1000 * 60], // minutes
+  ['s', 1000], // seconds
+  ['S', 1], // million seconds
+];
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  today = new Date();
+  lastDayOfMonth = new Date(this.today.getFullYear(), this.today.getMonth()+1, 0);
+  timeleft: any;
+  daysLeft: any;
+  sub: any;
+
+  config: CountdownConfig = {
+    leftTime: 0,
+    formatDate: ({ date, formatStr }) => {
+      let duration = Number(date || 0);
+
+      return CountdownTimeUnits.reduce((current, [name, unit]) => {
+        if (current.indexOf(name) !== -1) {
+          const v = Math.floor(duration / unit);
+          duration -= v * unit;
+          return current.replace(new RegExp(`${name}+`, 'g'), (match: string) => {
+            return v.toString().padStart(match.length, '0');
+          });
+        }
+        return current;
+      }, 'DD:HH:mm:ss');
+    },
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.daysLeft = this.lastDayOfMonth.getDate() - this.today.getDate()
+    console.log(this.today);
+    console.log(this.lastDayOfMonth);
+    console.log(this.lastDayOfMonth.getDate());
+    console.log(this.today.getDate());
+    this.timeleft = (this.lastDayOfMonth.getTime()/1000) - (this.today.getTime()/1000);
+    this.config.leftTime = this.timeleft;
+    console.log(this.timeleft);
+    this.updateDate();
+
+  }
+
+  updateDate(){
+    this.sub = interval(500)
+    .subscribe((val) => {
+      this.today = new Date();
+
+    });
+  }
+
+}
